01910247892d4a09f8f174802d0ae792ee0d4228
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-10-03 10:16:12 +0200

    feat: function return type

---
+++ src/07-types-for-arrays-problem.ts
@@ -0,0 +1,31 @@
+// TODO: fix typings for arrays
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  role: "admin" | "user" | "super-admin";
+  posts: Post[];
+}
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+export const defaultUser: User = {
+  id: 1,
+  firstName: "John",
+  lastName: "Doe",
+  role: "admin",
+  posts: [
+    {
+      id: 1,
+      title: "How I eat so much cheese",
+    },
+    {
+      id: 2,
+      title: "Why I don't eat more vegetables",
+    },
+  ],
+};
---
+++ src/08-function-return-type.ts
@@ -0,0 +1,54 @@
+import { expect, it } from "vitest";
+
+// TODO: fix typings for function return value
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  role: "admin" | "user" | "super-admin";
+  posts: Array<Post>;
+}
+
+interface Post {
+  id: number;
+  title: string;
+}
+
+/**
+ * How do we ensure that makeUser ALWAYS
+ * returns a user?
+ */
+// solution 1: copy user
+// solution 2: use "as"
+// solution 3: type function
+const makeUser = (): User => {
+  return {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    role: "admin",
+    posts: [
+      {
+        id: 1,
+        title: "How I eat so much cheese",
+      },
+      {
+        id: 2,
+        title: "Why I don't eat more vegetables",
+      },
+    ],
+  };
+  // return {} as User;
+};
+
+it("Should return a valid user", () => {
+  const user = makeUser();
+
+  expect(user.id).toBeTypeOf("number");
+  expect(user.firstName).toBeTypeOf("string");
+  expect(user.lastName).toBeTypeOf("string");
+  expect(user.role).to.be.oneOf(["super-admin", "admin", "user"]);
+
+  expect(user.posts[0].id).toBeTypeOf("number");
+  expect(user.posts[0].title).toBeTypeOf("string");
+});
